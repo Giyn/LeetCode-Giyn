f871a75027d7a13ef81b5c0aefce70aafb313ef7
许继元 <490601115@qq.com>
2021-09-23 20:58:30 +0800

    feat: submit 704

---
+++ Array/704_二分查找.py
@@ -0,0 +1,33 @@
+"""
+-------------------------------------
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/23 20:05:10
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 704_二分查找.py
+# @Software: PyCharm
+# @url     : https://leetcode-cn.com/problems/binary-search/
+-------------------------------------
+"""
+
+
+class Solution:
+    def search(self, nums, target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            middle = left + (right - left) // 2
+            if nums[middle] > target:
+                right = middle - 1
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+                return middle
+
+        return -1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.search([-1, 0, 3, 5, 9, 12], 9))
+    print(solution.search([-1, 0, 3, 5, 9, 12], 2))
