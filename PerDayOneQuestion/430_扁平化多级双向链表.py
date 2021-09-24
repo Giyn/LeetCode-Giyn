bd53ae3d131b837efd51fe2c878567ee8113cf12
许继元 <490601115@qq.com>
2021-09-24 15:22:46 +0800

    feat: submit 430

---
+++ PerDayOneQuestion/430_扁平化多级双向链表.py
@@ -0,0 +1,48 @@
+"""
+-------------------------------------
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/24 11:05:30
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 430_扁平化多级双向链表.py
+# @Software: PyCharm
+# @url     : https://leetcode-cn.com/problems/flatten-a-multilevel-doubly-linked-list/
+-------------------------------------
+"""
+
+
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+
+
+class Solution:
+    def flatten(self, head):
+        def dfs(node):
+            if not node:
+                return
+            if not node.child and not node.next:
+                return node
+            elif node.child:
+                last = dfs(node.child)
+                if last:
+                    last.next = node.next
+                if node.next:
+                    node.next.prev = last
+                node.next = node.child
+                node.child.prev = node
+                node.child = None
+                return dfs(last)
+            else:
+                return dfs(node.next)
+
+        dfs(head)
+
+        return head
+
+
+if __name__ == '__main__':
+    solution = Solution()
