e92f0ea70b0b9596d42f8e6fd66ac1b86485fe32
许继元 <490601115@qq.com>
2021-09-28 11:44:52 +0800

    feat: submit 326

---
+++ PerDayOneQuestion/326_3的幂.py
@@ -0,0 +1,26 @@
+"""
+-------------------------------------
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/28 11:36:43
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 326_3的幂.py
+# @Software: PyCharm
+# @url     : https://leetcode-cn.com/problems/power-of-three/
+-------------------------------------
+"""
+
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        while n > 0 and n % 3 == 0:
+            n = n // 3
+
+        return n == 1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.isPowerOfThree(27))
+    print(solution.isPowerOfThree(0))
+    print(solution.isPowerOfThree(9))
