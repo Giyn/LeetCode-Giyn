d0d4c58338dc59407831c4dc76417a61e7633d73
许继元 <490601115@qq.com>
2021-10-01 09:37:16 +0800

    feat: submit 367

---
+++ Array/367_有效的完全平方数.py
@@ -0,0 +1,37 @@
+"""
+-------------------------------------
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/30 23:08:20
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 367_有效的完全平方数.py
+# @Software: PyCharm
+# @url     : https://leetcode-cn.com/problems/valid-perfect-square/
+-------------------------------------
+"""
+
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        if num == 1:
+            return True
+
+        left = 1
+        right = num
+
+        while left <= right:
+            mid = left + ((right - left) >> 1)
+            if mid ** 2 == num:
+                return True
+            elif mid ** 2 > num:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return False
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.isPerfectSquare(16))
+    print(solution.isPerfectSquare(14))
