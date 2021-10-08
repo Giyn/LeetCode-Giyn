c9fae553b0e7dfaec30e32d6b443b4a51c1565f8
许继元 <490601115@qq.com>
2021-10-08 21:58:52 +0800

    feat: submit 673

---
+++ DynamicProgramming/673_最长递增子序列的个数.go
@@ -0,0 +1,49 @@
+/*
+-------------------------------------
+# @Time    : 2021/10/8 20:41:59
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 673_最长递增子序列的个数.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 3, 5, 4, 7}
+	//nums := []int{2,2,2,2,2}
+	fmt.Println(findNumberOfLIS(nums))
+}
+
+func findNumberOfLIS(nums []int) int {
+	maxLen := 0
+	ans := 0
+	n := len(nums)
+	dp := make([]int, n)  // 记录LIS长度
+	cnt := make([]int, n) // 记录LIS个数
+	for i, x := range nums {
+		dp[i] = 1
+		cnt[i] = 1
+		for j, y := range nums[:i] {
+			if x > y {
+				if dp[i] < dp[j]+1 {
+					dp[i] = dp[j] + 1
+					cnt[i] = cnt[j]
+				} else if dp[j]+1 == dp[i] {
+					cnt[i] += cnt[j]
+				}
+			}
+		}
+		if dp[i] > maxLen {
+			maxLen = dp[i]
+			ans = cnt[i]
+		} else if dp[i] == maxLen {
+			ans += cnt[i]
+		}
+	}
+
+	return ans
+}
