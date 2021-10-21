b8cb90118d3131260d5e1d2a0742e66c3fba95e5
许继元 <490601115@qq.com>
2021-10-22 00:39:44 +0800

    feat: submit 054

---
+++ Array/054_螺旋矩阵.go
@@ -0,0 +1,74 @@
+/*
+-------------------------------------
+# @Time    : 2021/10/20 10:54:24
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 054_螺旋矩阵.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	matrix := make([][]int, 3)
+	for i := range matrix {
+		matrix[i] = make([]int, 4)
+	}
+	matrix[0] = []int{1, 2, 3, 4}
+	matrix[1] = []int{5, 6, 7, 8}
+	matrix[2] = []int{9, 10, 11, 12}
+
+	fmt.Println(spiralOrder(matrix))
+}
+
+func spiralOrder(matrix [][]int) (ans []int) {
+	m, n := len(matrix), len(matrix[0])
+	amount := m * n
+	cnt := 0
+	top, bottom := 0, m-1
+	left, right := 0, n-1
+
+	for cnt < amount {
+		for i := left; i <= right; i++ {
+			if cnt < amount {
+				ans = append(ans, matrix[top][i])
+				cnt++
+			} else {
+				return
+			}
+		}
+		top++
+		for i := top; i <= bottom; i++ {
+			if cnt < amount {
+				ans = append(ans, matrix[i][right])
+				cnt++
+			} else {
+				return
+			}
+		}
+		right--
+		for i := right; i >= left; i-- {
+			if cnt < amount {
+				ans = append(ans, matrix[bottom][i])
+				cnt++
+			} else {
+				return
+			}
+		}
+		bottom--
+		for i := bottom; i >= top; i-- {
+			if cnt < amount {
+				ans = append(ans, matrix[i][left])
+				cnt++
+			} else {
+				return
+			}
+		}
+		left++
+	}
+
+	return ans
+}
