eeecd88d9ef31068343bd42aff03cbacf285b21d
许继元 <490601115@qq.com>
2021-11-04 17:24:26 +0800

    refactor: rename file

--- PerDayOneQuestion/剑指OfferII_069_山峰数组的顶部.go
+++ PerDayOneQuestion/剑指OfferII_069_山峰数组的顶部.go
@@ -3,7 +3,7 @@
 # @Time    : 2021/10/14 14:56:48
 # @Author  : Giyn
 # @Email   : giyn.jy@gmail.com
-# @File    : 剑指Offer_II_069_山峰数组的顶部.go
+# @File    : 剑指OfferII_069_山峰数组的顶部.go
 # @Software: GoLand
 -------------------------------------
 */
@@ -20,13 +20,13 @@ func main() {
 func peakIndexInMountainArray(arr []int) int {
 	left, right := 1, len(arr)-2
 	for left <= right {
-		mid := left + (right - left) >> 1
+		mid := left + (right-left)>>1
 		if arr[mid-1] < arr[mid] && arr[mid+1] < arr[mid] {
 			return mid
 		} else if arr[mid-1] < arr[mid] {
 			left = mid + 1
 		} else {
-			right = mid -1
+			right = mid - 1
 		}
 	}
 	return right
