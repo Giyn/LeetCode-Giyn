fbfe5305e4aa5671a4504d76e5b9e49ee626ef2b
许继元 <490601115@qq.com>
2021-11-09 21:05:33 +0800

    perf: encapsulate a function

--- StackAndQueue/232_用栈实现队列.go
+++ StackAndQueue/232_用栈实现队列.go
@@ -37,13 +37,16 @@ func (this *MyQueue) Push(x int) {
 	this.inStack = append(this.inStack, x)
 }
 
+func (this *MyQueue) in2out() {
+	for len(this.inStack) != 0 {
+		this.outStack = append(this.outStack, this.inStack[len(this.inStack)-1])
+		this.inStack = this.inStack[:len(this.inStack)-1]
+	}
+}
+
 func (this *MyQueue) Pop() int {
 	if len(this.outStack) == 0 {
-		for len(this.inStack) != 0 {
-			val := this.inStack[len(this.inStack)-1]
-			this.inStack = this.inStack[:len(this.inStack)-1]
-			this.outStack = append(this.outStack, val)
-		}
+		this.in2out()
 	}
 	val := this.outStack[len(this.outStack)-1]
 	this.outStack = this.outStack[:len(this.outStack)-1]
@@ -52,14 +55,9 @@ func (this *MyQueue) Pop() int {
 
 func (this *MyQueue) Peek() int {
 	if len(this.outStack) == 0 {
-		for len(this.inStack) != 0 {
-			val := this.inStack[len(this.inStack)-1]
-			this.inStack = this.inStack[:len(this.inStack)-1]
-			this.outStack = append(this.outStack, val)
-		}
+		this.in2out()
 	}
-	val := this.outStack[len(this.outStack)-1]
-	return val
+	return this.outStack[len(this.outStack)-1]
 }
 
 func (this *MyQueue) Empty() bool {
