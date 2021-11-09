29eedf6d155b15793f9865108410ac7be5a468a3
许继元 <490601115@qq.com>
2021-11-10 00:24:11 +0800

    feat: submit 495

---
+++ PerDayOneQuestion/495_提莫攻击.go
@@ -0,0 +1,32 @@
+/*
+-------------------------------------
+# @Time    : 2021/11/10 0:03:52
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 495_提莫攻击.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	timeSeries := []int{1, 2}
+	duration := 2
+	fmt.Println(findPoisonedDuration(timeSeries, duration))
+}
+
+func findPoisonedDuration(timeSeries []int, duration int) (ans int) {
+	for i := 1; i < len(timeSeries); i++ {
+		diff := timeSeries[i] - timeSeries[i-1]
+		if diff >= duration {
+			ans += duration
+		} else {
+			ans += diff
+		}
+	}
+	ans += duration
+	return
+}
