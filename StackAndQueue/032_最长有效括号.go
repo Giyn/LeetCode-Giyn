37513fb135a9ad729737355093ceb139d789f026
许继元 <490601115@qq.com>
2021-11-10 21:55:18 +0800

    perf: O(1) space

--- StackAndQueue/032_最长有效括号.go
+++ StackAndQueue/032_最长有效括号.go
@@ -13,26 +13,57 @@ package main
 import "fmt"
 
 func main() {
-	s := ")()())"
+	s := "(()))())("
 	fmt.Println(longestValidParentheses(s))
 }
 
 func longestValidParentheses(s string) int {
-	stack := []int{-1}
 	max := 0
-	for i, ch := range s {
-		if ch == '(' {
-			stack = append(stack, i)
+	left, right := 0, 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			left++
 		} else {
-			stack = stack[:len(stack)-1]
-			if len(stack) == 0 {
-				stack = append(stack, i)
-			}
-			max = max032(max, i-stack[len(stack)-1])
+			right++
+		}
+		if left < right {
+			left, right = 0, 0
+		} else if left == right {
+			max = max032(max, 2*left)
+		}
+	}
+	left, right = 0, 0
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '(' {
+			left++
+		} else {
+			right++
+		}
+		if left > right {
+			left, right = 0, 0
+		} else if left == right {
+			max = max032(max, 2*right)
 		}
 	}
 	return max
 }
+
+//func longestValidParentheses(s string) int {
+//	stack := []int{-1}
+//	max := 0
+//	for i, ch := range s {
+//		if ch == '(' {
+//			stack = append(stack, i)
+//		} else {
+//			stack = stack[:len(stack)-1]
+//			if len(stack) == 0 {
+//				stack = append(stack, i)
+//			}
+//			max = max032(max, i-stack[len(stack)-1])
+//		}
+//	}
+//	return max
+//}
 
 func max032(x, y int) int {
 	if x > y {
