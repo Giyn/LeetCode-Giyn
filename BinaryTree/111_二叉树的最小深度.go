07fdcea0d7f6217ab0092117c076dbadb9c1aa41
许继元 <490601115@qq.com>
2021-11-18 01:02:37 +0800

    feat: add recursion solution

--- BinaryTree/111_二叉树的最小深度.go
+++ BinaryTree/111_二叉树的最小深度.go
@@ -11,7 +11,6 @@
 package main
 
 import (
-	"container/list"
 	"fmt"
 )
 
@@ -21,33 +20,53 @@ func main() {
 }
 
 func minDepth(root *TreeNode111) int {
+	// 递归
 	if root == nil {
 		return 0
 	}
-	depth := 0
-	queue := list.New()
-	queue.PushBack(root)
-	for queue.Len() > 0 {
-		length := queue.Len()
-		depth++
-		for i := 0; i < length; i++ {
-			node := queue.Remove(queue.Front()).(*TreeNode111)
-			if node.Left != nil {
-				queue.PushBack(node.Left)
-			}
-			if node.Right != nil {
-				queue.PushBack(node.Right)
-			}
-			if node.Left == nil && node.Right == nil {
-				return depth
-			}
-		}
+	if root.Left != nil && root.Right == nil {
+		return 1 + minDepth(root.Left)
 	}
-	return depth
+	if root.Left == nil && root.Right != nil {
+		return 1 + minDepth(root.Right)
+	}
+	return 1 + min111(minDepth(root.Left), minDepth(root.Right))
+
+	// 迭代
+	//if root == nil {
+	//	return 0
+	//}
+	//depth := 0
+	//queue := list.New()
+	//queue.PushBack(root)
+	//for queue.Len() > 0 {
+	//	length := queue.Len()
+	//	depth++
+	//	for i := 0; i < length; i++ {
+	//		node := queue.Remove(queue.Front()).(*TreeNode111)
+	//		if node.Left != nil {
+	//			queue.PushBack(node.Left)
+	//		}
+	//		if node.Right != nil {
+	//			queue.PushBack(node.Right)
+	//		}
+	//		if node.Left == nil && node.Right == nil {
+	//			return depth
+	//		}
+	//	}
+	//}
+	//return depth
 }
 
 type TreeNode111 struct {
 	Val   int
 	Left  *TreeNode111
 	Right *TreeNode111
+}
+
+func min111(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
