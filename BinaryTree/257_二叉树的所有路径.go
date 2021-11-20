6efdcfc650fdb3af39ad2fc3301030d826a52cae
许继元 <490601115@qq.com>
2021-11-20 10:53:45 +0800

    feat: submit 257

---
+++ BinaryTree/257_二叉树的所有路径.go
@@ -0,0 +1,47 @@
+/*
+-------------------------------------
+# @Time    : 2021/11/18 21:53:25
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 257_二叉树的所有路径.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	root := &TreeNode257{1, &TreeNode257{2, nil, &TreeNode257{5, nil, nil}}, &TreeNode257{3, nil, nil}}
+	fmt.Println(binaryTreePaths(root))
+}
+
+func binaryTreePaths(root *TreeNode257) (ans []string) {
+	var travel func(node *TreeNode257, s string)
+	travel = func(node *TreeNode257, s string) {
+		if node.Left == nil && node.Right == nil {
+			v := s + strconv.Itoa(node.Val)
+			ans = append(ans, v)
+			return
+		}
+		s = s + strconv.Itoa(node.Val) + "->"
+		if node.Left != nil {
+			travel(node.Left, s)
+		}
+		if node.Right != nil {
+			travel(node.Right, s)
+		}
+	}
+	travel(root, "")
+	return
+}
+
+type TreeNode257 struct {
+	Val   int
+	Left  *TreeNode257
+	Right *TreeNode257
+}
