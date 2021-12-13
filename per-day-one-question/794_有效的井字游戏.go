c342e6e2c05a00f283ed37c70fc791140a0b5395
许继元 <490601115@qq.com>
2021-12-13 09:48:58 +0800

    feat: submit 794

---
+++ per-day-one-question/794_有效的井字游戏.go
@@ -0,0 +1,43 @@
+/*
+-------------------------------------
+# @Time    : 2021/12/13 8:54:13
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 794_有效的井字游戏.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	board := []string{"XOX", " X ", "   "}
+	fmt.Println(validTicTacToe(board))
+}
+
+func win(board []string, p byte) bool {
+	for i := 0; i < 3; i++ {
+		if board[i][0] == p && board[i][1] == p && board[i][2] == p ||
+			board[0][i] == p && board[1][i] == p && board[2][i] == p {
+			return true
+		}
+	}
+	return board[0][0] == p && board[1][1] == p && board[2][2] == p ||
+		board[0][2] == p && board[1][1] == p && board[2][0] == p
+}
+
+func validTicTacToe(board []string) bool {
+	oCount, xCount := 0, 0
+	for _, row := range board {
+		oCount += strings.Count(row, "O")
+		xCount += strings.Count(row, "X")
+	}
+	return !(oCount != xCount && oCount != xCount-1 ||
+		oCount != xCount && win(board, 'O') ||
+		oCount != xCount-1 && win(board, 'X'))
+}
