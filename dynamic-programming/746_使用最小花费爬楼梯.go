e9ddfc1255d8e9377863aa22ca9452e06b6073ba
许继元 <490601115@qq.com>
2022-01-20 19:15:14 +0800

    feat: submit 746

---
+++ dynamic-programming/746_使用最小花费爬楼梯.go
@@ -0,0 +1,36 @@
+/*
+-------------------------------------
+# @Time    : 2022/1/20 17:51:48
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 746_使用最小花费爬楼梯.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	cost := []int{10, 15, 20}
+	fmt.Println(minCostClimbingStairs(cost))
+}
+
+func minCostClimbingStairs(cost []int) int {
+	var min func(int, int) int
+	min = func(x, y int) int {
+		if x < y {
+			return x
+		}
+		return y
+	}
+	n := len(cost)
+	dp := make([]int, n)
+	dp[0] = cost[0]
+	dp[1] = cost[1]
+	for i := 2; i < n; i++ {
+		dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+	}
+	return min(dp[n-1], dp[n-2])
+}
