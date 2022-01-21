e73cd925294c1746550292a5ef83b8584f510f32
许继元 <490601115@qq.com>
2022-01-21 18:55:46 +0800

    feat: submit 063

---
+++ dynamic-programming/063_不同路径 II.go
@@ -0,0 +1,40 @@
+/*
+-------------------------------------
+# @Time    : 2022/1/21 17:02:57
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 063_不同路径 II.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	obstacleGrid := [][]int{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}
+	fmt.Println(uniquePathsWithObstacles(obstacleGrid))
+}
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	m, n := len(obstacleGrid), len(obstacleGrid[0])
+	dp := make([][]int, m)
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+	for i := 0; i < m && obstacleGrid[i][0] == 0; i++ {
+		dp[i][0] = 1
+	}
+	for j := 0; j < n && obstacleGrid[0][j] == 0; j++ {
+		dp[0][j] = 1
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if obstacleGrid[i][j] == 0 {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			}
+		}
+	}
+	return dp[m-1][n-1]
+}
