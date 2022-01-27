4b888e2f941b4c0f5295aef7e32f14fc0ad98e04
许继元 <490601115@qq.com>
2022-01-27 16:47:58 +0800

    feat: submit 070

--- dynamic-programming/070_爬楼梯.go
+++ dynamic-programming/070_爬楼梯.go
@@ -18,14 +18,25 @@ func main() {
 }
 
 func climbStairs(n int) int {
-	dp := []int{0, 1, 2}
-	if n <= 2 {
-		return n
+	//dp := []int{0, 1, 2}
+	//if n <= 2 {
+	//	return n
+	//}
+	//for i := 2; i < n; i++ {
+	//	tmp := dp[1] + dp[2]
+	//	dp[1] = dp[2]
+	//	dp[2] = tmp
+	//}
+	//return dp[2]
+
+	dp := make([]int, n+1)
+	dp[0] = 1
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= 2; j++ {
+			if i >= j {
+				dp[i] += dp[i-j]
+			}
+		}
 	}
-	for i := 2; i < n; i++ {
-		tmp := dp[1] + dp[2]
-		dp[1] = dp[2]
-		dp[2] = tmp
-	}
-	return dp[2]
+	return dp[n]
 }
