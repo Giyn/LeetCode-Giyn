95b74f9b8706bcc0b1cf4712d62ccd840a058364
许继元 <490601115@qq.com>
2022-02-11 12:27:36 +0800

    feat: submit 1405

--- per-day-one-question/1405_最长快乐字符串.go
+++ per-day-one-question/1405_最长快乐字符串.go
@@ -11,8 +11,8 @@
 package main
 
 import (
-	"container/heap"
 	"fmt"
+	"sort"
 )
 
 func main() {
@@ -20,59 +20,30 @@ func main() {
 	fmt.Println(longestDiverseString(a, b, c))
 }
 
-type CharNum struct {
-	val  int
-	char byte
-}
-
-func longestDiverseString(a int, b int, c int) string {
-	pq, ans := &IntHeap{}, []byte{}
-	heap.Push(pq, CharNum{a, 'a'})
-	heap.Push(pq, CharNum{b, 'b'})
-	heap.Push(pq, CharNum{c, 'c'})
-	for pq.Len() > 0 {
-		v := addChar(pq, ans)
-		if len(ans) == len(v) {
+func longestDiverseString(a, b, c int) string {
+	var ans []byte
+	cnt := []struct {
+		num int
+		ch  byte
+	}{{a, 'a'}, {b, 'b'}, {c, 'c'}}
+	for {
+		sort.Slice(cnt, func(i, j int) bool { return cnt[i].num > cnt[j].num })
+		hasNext := false
+		for i, pair := range cnt {
+			if pair.num <= 0 {
+				break
+			}
+			m := len(ans)
+			if m >= 2 && ans[m-2] == pair.ch && ans[m-1] == pair.ch {
+				continue
+			}
+			hasNext = true
+			ans = append(ans, pair.ch)
+			cnt[i].num--
 			break
 		}
-		ans = v
-	}
-	return string(ans)
-}
-
-func addChar(pq *IntHeap, ans []byte) []byte {
-	cur := heap.Pop(pq).(CharNum)
-	if cur.val == 0 {
-		return ans
-	}
-	if len(ans) >= 2 && cur.char == ans[len(ans)-1] && cur.char == ans[len(ans)-2] {
-		if pq.Len() == 0 {
-			return ans
+		if !hasNext {
+			return string(ans)
 		}
-		v := addChar(pq, ans)
-		if len(ans) == len(v) {
-			return ans
-		}
-		ans = v
 	}
-	ans = append(ans, cur.char)
-	cur.val--
-	heap.Push(pq, cur)
-	return ans
-}
-
-type IntHeap []CharNum
-
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i].val > h[j].val }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *IntHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
-func (h *IntHeap) Push(x interface{}) {
-	*h = append(*h, x.(CharNum))
 }
