64867ed84e55a53ce59068db3b409da91deb3a62
许继元 <490601115@qq.com>
2022-02-24 15:07:45 +0800

    feat: submit 503

--- monotone-stack/503_下一个更大元素 II.go
+++ monotone-stack/503_下一个更大元素 II.go
@@ -18,29 +18,40 @@ func main() {
 }
 
 func nextGreaterElements(nums []int) (ans []int) {
+	// 模拟循环遍历
 	n := len(nums)
+	ans = make([]int, n)
+	for i := range ans {
+		ans[i] = -1
+	}
 	var stack []int
-
-	for i := 0; i < n-1; i++ {
-		nums = append(nums, nums[i])
-	}
-	for i := len(nums) - 1; i >= 0; i-- {
-		for len(stack) > 0 && nums[i] >= stack[len(stack)-1] {
+	for i := 0; i < 2*n; i++ {
+		for len(stack) > 0 && nums[i%n] > nums[stack[len(stack)-1]] {
+			top := stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
+			ans[top] = nums[i%n]
 		}
-		if len(stack) == 0 {
-			ans = append(ans, -1)
-		} else {
-			ans = append(ans, stack[len(stack)-1])
-		}
-		stack = append(stack, nums[i])
+		stack = append(stack, i%n)
 	}
-	return reverse(ans)[:n]
-}
+	return
 
-func reverse(s []int) []int {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
-	}
-	return s
+	// 修改数组
+	//n := len(nums)
+	//var stack []int
+	//ans = make([]int, 2*n)
+	//for i := 0; i < n-1; i++ {
+	//	nums = append(nums, nums[i])
+	//}
+	//for i := range ans {
+	//	ans[i] = -1
+	//}
+	//for i := 0; i < len(nums); i++ {
+	//	for len(stack) > 0 && nums[i] > nums[stack[len(stack)-1]] {
+	//		top := stack[len(stack)-1]
+	//		stack = stack[:len(stack)-1]
+	//		ans[top] = nums[i]
+	//	}
+	//	stack = append(stack, i)
+	//}
+	//return ans[:n]
 }
