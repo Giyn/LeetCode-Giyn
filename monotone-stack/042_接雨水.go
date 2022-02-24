66c4c32a3df3a62d7418218b554b53d332cca89e
许继元 <490601115@qq.com>
2022-02-24 15:26:24 +0800

    feat: submit 042

--- monotone-stack/042_接雨水.go
+++ monotone-stack/042_接雨水.go
@@ -10,7 +10,10 @@
 
 package main
 
-import "fmt"
+import (
+	"LeetCodeGiyn/utils"
+	"fmt"
+)
 
 func main() {
 	height := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
@@ -19,51 +22,41 @@ func main() {
 }
 
 func trap(height []int) (ans int) {
-	var stack []int
-	for i := 0; i < len(height); i++ {
-		for len(stack) > 0 && height[stack[len(stack)-1]] < height[i] {
-			cur := stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
-			if len(stack) == 0 {
-				break
-			}
-			left := stack[len(stack)-1]
-			right := i
-			h := min042(height[left], height[right]) - height[cur]
-			ans += (right - left - 1) * h
-		}
-		stack = append(stack, i)
+	// 动态规划
+	n := len(height)
+	if n < 3 {
+		return
+	}
+	maxLeft := make([]int, n)
+	maxRight := make([]int, n)
+	maxLeft[0] = height[0]
+	maxRight[n-1] = height[n-1]
+	for i := 1; i < n; i++ {
+		maxLeft[i] = utils.Max(maxLeft[i-1], height[i])
+	}
+	for i := n - 2; i >= 0; i-- {
+		maxRight[i] = utils.Max(maxRight[i+1], height[i])
+	}
+	for i := 0; i < n; i++ {
+		ans += utils.Min(maxLeft[i], maxRight[i]) - height[i]
 	}
 	return
+
+	// 单调栈
+	//var stack []int
+	//for i := 0; i < len(height); i++ {
+	//	for len(stack) > 0 && height[stack[len(stack)-1]] < height[i] {
+	//		cur := stack[len(stack)-1]
+	//		stack = stack[:len(stack)-1]
+	//		if len(stack) == 0 {
+	//			break
+	//		}
+	//		left := stack[len(stack)-1]
+	//		right := i
+	//		h := utils.Min(height[left], height[right]) - height[cur]
+	//		ans += (right - left - 1) * h
+	//	}
+	//	stack = append(stack, i)
+	//}
+	//return
 }
-
-func min042(x, y int) int {
-	if x > y {
-		return y
-	} else {
-		return x
-	}
-}
-
-// 暴力解法
-//func trap(height []int) (ans int) {
-//	for i := 1; i < len(height)-1; i++ {
-//		maxLeft, maxRight := 0, 0
-//		for j := i; j >= 0; j-- {
-//			maxLeft = max042(maxLeft, height[j])
-//		}
-//		for j := i; j < len(height); j++ {
-//			maxRight = max042(maxRight, height[j])
-//		}
-//		ans += min042(maxLeft, maxRight) - height[i]
-//	}
-//	return
-//}
-
-//func max042(x, y int) int {
-//	if x > y {
-//		return x
-//	} else {
-//		return y
-//	}
-//}
