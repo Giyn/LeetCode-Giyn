1fdbd6c85d44414b17858e4e2e60ebb3f7947ab8
许继元 <490601115@qq.com>
2022-02-26 10:42:39 +0800

    feat: submit 042

--- monotone-stack/042_接雨水.go
+++ monotone-stack/042_接雨水.go
@@ -22,41 +22,48 @@ func main() {
 }
 
 func trap(height []int) (ans int) {
-	// 动态规划
-	n := len(height)
-	if n < 3 {
-		return
-	}
-	maxLeft := make([]int, n)
-	maxRight := make([]int, n)
-	maxLeft[0] = height[0]
-	maxRight[n-1] = height[n-1]
-	for i := 1; i < n; i++ {
-		maxLeft[i] = utils.Max(maxLeft[i-1], height[i])
-	}
-	for i := n - 2; i >= 0; i-- {
-		maxRight[i] = utils.Max(maxRight[i+1], height[i])
-	}
-	for i := 0; i < n; i++ {
-		ans += utils.Min(maxLeft[i], maxRight[i]) - height[i]
+	// 单调栈
+	var stack []int
+	stack = append(stack, 0)
+	for i := 1; i < len(height); i++ {
+		if height[i] < height[stack[len(stack)-1]] {
+			stack = append(stack, i)
+		} else if height[i] == height[stack[len(stack)-1]] {
+			stack = stack[:len(stack)-1]
+			stack = append(stack, i)
+		} else {
+			for len(stack) > 0 && height[stack[len(stack)-1]] < height[i] {
+				mid := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				if len(stack) > 0 {
+					left := stack[len(stack)-1]
+					right := i
+					h := utils.Min(height[left], height[right]) - height[mid]
+					ans += (right - left - 1) * h
+				}
+			}
+			stack = append(stack, i)
+		}
 	}
 	return
 
-	// 单调栈
-	//var stack []int
-	//for i := 0; i < len(height); i++ {
-	//	for len(stack) > 0 && height[stack[len(stack)-1]] < height[i] {
-	//		cur := stack[len(stack)-1]
-	//		stack = stack[:len(stack)-1]
-	//		if len(stack) == 0 {
-	//			break
-	//		}
-	//		left := stack[len(stack)-1]
-	//		right := i
-	//		h := utils.Min(height[left], height[right]) - height[cur]
-	//		ans += (right - left - 1) * h
-	//	}
-	//	stack = append(stack, i)
+	// 动态规划
+	//n := len(height)
+	//if n < 3 {
+	//	return
+	//}
+	//maxLeft := make([]int, n)
+	//maxRight := make([]int, n)
+	//maxLeft[0] = height[0]
+	//maxRight[n-1] = height[n-1]
+	//for i := 1; i < n; i++ {
+	//	maxLeft[i] = utils.Max(maxLeft[i-1], height[i])
+	//}
+	//for i := n - 2; i >= 0; i-- {
+	//	maxRight[i] = utils.Max(maxRight[i+1], height[i])
+	//}
+	//for i := 0; i < n; i++ {
+	//	ans += utils.Min(maxLeft[i], maxRight[i]) - height[i]
 	//}
 	//return
 }
