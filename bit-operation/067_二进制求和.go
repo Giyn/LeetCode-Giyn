eae306f5e3456a4ae572cc9c9aae35ba83aa4ec8
许继元 <490601115@qq.com>
2022-03-03 11:16:13 +0800

    feat: submit 067

---
+++ bit-operation/067_二进制求和.go
@@ -0,0 +1,44 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/3 10:14:53
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 067_二进制求和.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	"LeetCodeGiyn/utils"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	a := "1010"
+	b := "1011"
+	fmt.Println(addBinary(a, b))
+}
+
+func addBinary(a string, b string) (ans string) {
+	carry := 0
+	lenA, lenB := len(a), len(b)
+	n := utils.Max(lenA, lenB)
+
+	for i := 0; i < n; i++ {
+		if i < lenA {
+			carry += int(a[lenA-1-i] - '0')
+		}
+		if i < lenB {
+			carry += int(b[lenB-1-i] - '0')
+		}
+		ans = strconv.Itoa(carry%2) + ans
+		carry /= 2
+	}
+	if carry > 0 {
+		ans = "1" + ans
+	}
+	return
+}
