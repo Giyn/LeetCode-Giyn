278676606a68083cc69d6d96d92728cecdccb011
许继元 <490601115@qq.com>
2022-03-07 11:52:17 +0800

    feat: submit 008

---
+++ 剑指Offer-II/剑指OfferII_008_和大于等于 target 的最短子数组.go
@@ -0,0 +1,45 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/7 0:40:46
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 剑指OfferII_008_和大于等于 target 的最短子数组.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	"LeetCodeGiyn/utils"
+	"fmt"
+	"math"
+)
+
+func main() {
+	target := 7
+	nums := []int{2, 3, 1, 2, 4, 3}
+	fmt.Println(minSubArrayLen(target, nums))
+}
+
+func minSubArrayLen(target int, nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	ans := math.MaxInt64
+	left, right := 0, 0
+	sum := 0
+	for right < len(nums) {
+		sum += nums[right]
+		for sum >= target {
+			ans = utils.Min(ans, right-left+1)
+			sum -= nums[left]
+			left++
+		}
+		right++
+	}
+	if ans == math.MaxInt64 {
+		return 0
+	}
+	return ans
+}
