41d1bebfb41ce8553421e00810298e7ed05d44c1
许继元 <490601115@qq.com>
2022-03-09 15:49:36 +0800

    feat: submit 003

---
+++ sliding-window/003_无重复字符的最长子串.go
@@ -0,0 +1,39 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/9 15:47:53
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 003_无重复字符的最长子串.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	"LeetCodeGiyn/utils"
+	"fmt"
+)
+
+func main() {
+	s := "abcabcbb"
+	fmt.Println(lengthOfLongestSubstring(s))
+}
+
+func lengthOfLongestSubstring(s string) (ans int) {
+	n := len(s)
+	mp := make(map[byte]bool, n)
+	right := 0
+	for i := 0; i < n; i++ {
+		if i > 0 {
+			// 起始字母前移
+			delete(mp, s[i-1])
+		}
+		for right < n && !mp[s[right]] {
+			mp[s[right]] = true
+			right++
+		}
+		ans = utils.Max(ans, right-i)
+	}
+	return ans
+}
