0c9aad4f65396105a19c3c11e34b628a33adc726
许继元 <490601115@qq.com>
2022-03-11 19:55:53 +0800

    feat: submit node

--- utils/linked-list/listnode.go
+++ utils/linked-list/listnode.go
@@ -15,12 +15,28 @@ type ListNode struct {
 	Next *ListNode
 }
 
+type Node struct {
+	Val  int
+	Next *Node
+}
+
 func NewListNode(vals []int) *ListNode {
 	var head *ListNode
 	cur := new(ListNode)
 	head = cur
 	for _, val := range vals {
 		cur.Next = &ListNode{val, nil}
+		cur = cur.Next
+	}
+	return head.Next
+}
+
+func NewNode(vals []int) *Node {
+	var head *Node
+	cur := new(Node)
+	head = cur
+	for _, val := range vals {
+		cur.Next = &Node{val, nil}
 		cur = cur.Next
 	}
 	return head.Next
