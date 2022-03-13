849e715107bbba5609ad76524661adfd51c9b987
许继元 <490601115@qq.com>
2022-03-13 10:46:27 +0800

    feat: submit doubly linked list

---
+++ utils/doubly-linked-list/node.go
@@ -0,0 +1,11 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/12 15:38:14
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : node.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package doubly_linked_list
