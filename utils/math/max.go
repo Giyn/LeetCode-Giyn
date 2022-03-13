5bb088f834f062107d192ac23289c528f1605ef5
许继元 <490601115@qq.com>
2022-03-13 15:00:35 +0800

    feat: add slice

--- utils/math/max.go
+++ utils/math/max.go
@@ -10,11 +10,23 @@
 
 package math
 
-func Max(args ...int) (ans int) {
-	ans = args[0]
-	for _, arg := range args[1:] {
-		if arg > ans {
-			ans = arg
+import "math"
+
+func Max(args ...interface{}) (ans int) {
+	ans = math.MinInt64
+	for _, arg := range args {
+		switch arg.(type) {
+		case []int:
+			for i := 0; i < len(arg.([]int)); i++ {
+				if arg.([]int)[i] > ans {
+					ans = arg.([]int)[i]
+				}
+			}
+			return
+		case int:
+			if arg.(int) > ans {
+				ans = arg.(int)
+			}
 		}
 	}
 	return
---
+++ utils/math/max_test.go
@@ -0,0 +1,21 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/13 13:20:16
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : max_test.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package math
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewMax(t *testing.T) {
+	fmt.Println(Max([]int{1, 2, 3, 4, 6}))
+	fmt.Println(Max(1, 2, 3, 4, 6))
+}
