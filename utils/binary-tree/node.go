7d441e29f4faf675be6813524dc113fa58e6f9a8
许继元 <490601115@qq.com>
2022-03-13 10:46:12 +0800

    feat: submit binary tree

---
+++ utils/binary-tree/node.go
@@ -0,0 +1,17 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/13 10:03:44
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : node.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package binary_tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
