a0d8e6dbc67c56651b2db0ecf7c559d66c1e006c
许继元 <490601115@qq.com>
2022-03-15 21:50:36 +0800

    feat: submit 373

--- priority-queue/373_查找和最小的 K 对数字.go
+++ priority-queue/373_查找和最小的 K 对数字.go
@@ -26,22 +26,21 @@ func kSmallestPairs(nums1, nums2 []int, k int) (ans [][]int) {
 	m, n := len(nums1), len(nums2)
 	h := hp373{nil, nums1, nums2}
 	for i := 0; i < k && i < m; i++ {
-		h.data = append(h.data, pair373{i, 0})
+		h.data = append(h.data, struct{ i, j int }{i, 0})
 	}
 	for h.Len() > 0 && len(ans) < k {
-		p := heap.Pop(&h).(pair373)
+		p := heap.Pop(&h).(struct{ i, j int })
 		i, j := p.i, p.j
 		ans = append(ans, []int{nums1[i], nums2[j]})
 		if j+1 < n {
-			heap.Push(&h, pair373{i, j + 1})
+			heap.Push(&h, struct{ i, j int }{i, j + 1})
 		}
 	}
 	return
 }
 
-type pair373 struct{ i, j int }
 type hp373 struct {
-	data         []pair373
+	data         []struct{ i, j int }
 	nums1, nums2 []int
 }
 
@@ -51,5 +50,5 @@ func (h hp373) Less(i, j int) bool {
 	return h.nums1[a.i]+h.nums2[a.j] < h.nums1[b.i]+h.nums2[b.j]
 }
 func (h hp373) Swap(i, j int)       { h.data[i], h.data[j] = h.data[j], h.data[i] }
-func (h *hp373) Push(v interface{}) { h.data = append(h.data, v.(pair373)) }
+func (h *hp373) Push(v interface{}) { h.data = append(h.data, v.(struct{ i, j int })) }
 func (h *hp373) Pop() interface{}   { a := h.data; v := a[len(a)-1]; h.data = a[:len(a)-1]; return v }
