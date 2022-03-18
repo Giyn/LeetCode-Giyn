40f71ebbd1f29fe72ae2288984aa9af6d52cd58c
许继元 <490601115@qq.com>
2022-03-18 09:02:19 +0800

    feat: submit 695

---
+++ dfs-bfs/695_岛屿的最大面积.go
@@ -0,0 +1,54 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/18 0:35:01
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 695_岛屿的最大面积.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	. "LeetCodeGiyn/utils/math"
+	"fmt"
+)
+
+func main() {
+	grid := [][]int{
+		{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+		{0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+		{0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0},
+	}
+	fmt.Println(maxAreaOfIsland(grid))
+}
+
+func maxAreaOfIsland(grid [][]int) (ans int) {
+	var dfs func(grid [][]int, i, j int) int
+	dfs = func(grid [][]int, i, j int) int {
+		if i < 0 || j < 0 || i >= len(grid) || j >= len(grid[0]) || grid[i][j] == 0 {
+			return 0
+		}
+		area := 1
+		grid[i][j] = 0
+		area += dfs(grid, i+1, j)
+		area += dfs(grid, i-1, j)
+		area += dfs(grid, i, j+1)
+		area += dfs(grid, i, j-1)
+		return area
+	}
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if grid[i][j] == 1 {
+				ans = Max(ans, dfs(grid, i, j))
+			}
+		}
+	}
+	return
+}
