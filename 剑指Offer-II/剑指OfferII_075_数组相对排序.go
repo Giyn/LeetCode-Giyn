804fc66435d36c6c804d980cec25eebc3048cccc
许继元 <490601115@qq.com>
2022-03-18 09:03:00 +0800

    feat: submit 075

---
+++ 剑指Offer-II/剑指OfferII_075_数组相对排序.go
@@ -0,0 +1,40 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/17 0:30:20
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 剑指OfferII_075_数组相对排序.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	arr1 := []int{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}
+	arr2 := []int{2, 1, 4, 3, 9, 6}
+	fmt.Println(relativeSortArray(arr1, arr2))
+}
+
+func relativeSortArray(arr1 []int, arr2 []int) []int {
+	myRank := map[int]int{}
+	for i, v := range arr2 {
+		myRank[v] = i - len(arr2)
+	}
+	sort.Slice(arr1, func(i, j int) bool {
+		x, y := arr1[i], arr1[j]
+		if r, ok := myRank[x]; ok {
+			x = r
+		}
+		if r, ok := myRank[y]; ok {
+			y = r
+		}
+		return x < y
+	})
+	return arr1
+}
