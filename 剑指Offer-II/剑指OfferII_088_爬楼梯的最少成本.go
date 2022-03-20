770b2b78290f82117ff5615e2d5e3962535e1a3f
许继元 <490601115@qq.com>
2022-03-21 00:23:23 +0800

    feat: submit 088

---
+++ 剑指Offer-II/剑指OfferII_088_爬楼梯的最少成本.go
@@ -0,0 +1,32 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/20 13:12:16
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 剑指OfferII_088_爬楼梯的最少成本.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	. "LeetCodeGiyn/utils/math"
+	"fmt"
+)
+
+func main() {
+	cost := []int{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}
+	fmt.Println(minCostClimbingStairs(cost))
+}
+
+func minCostClimbingStairs(cost []int) int {
+	dp := make([]int, len(cost))
+	dp[0] = cost[0]
+	dp[1] = cost[1]
+
+	for i := 2; i < len(cost); i++ {
+		dp[i] = Min(dp[i-1], dp[i-2]) + cost[i]
+	}
+	return Min(dp[len(dp)-1], dp[len(dp)-2])
+}
