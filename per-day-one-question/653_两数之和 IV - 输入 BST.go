42459de1ea1f64834b59edd30f610177d02857b6
许继元 <490601115@qq.com>
2022-03-21 00:53:38 +0800

    feat: submit 653

---
+++ per-day-one-question/653_两数之和 IV - 输入 BST.go
@@ -0,0 +1,38 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/21 0:19:50
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 653_两数之和 IV - 输入 BST.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	. "LeetCodeGiyn/utils/binary-tree"
+	"fmt"
+)
+
+func main() {
+	root := NewTreeNode([]int{8, 6, 10, 5, 7, 9, 11})
+	k := 12
+	fmt.Println(findTarget(root, k))
+}
+
+func findTarget(root *TreeNode, k int) bool {
+	mp := make(map[int]bool)
+	var dfs func(node *TreeNode) bool
+	dfs = func(node *TreeNode) bool {
+		if node == nil {
+			return false
+		}
+		if mp[k-node.Val] {
+			return true
+		}
+		mp[node.Val] = true
+		return dfs(node.Left) || dfs(node.Right)
+	}
+	return dfs(root)
+}
