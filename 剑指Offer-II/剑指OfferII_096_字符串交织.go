956e2f3e2bc0b9cb18e2bfa0e94598b2391de232
许继元 <490601115@qq.com>
2022-03-21 00:24:00 +0800

    feat: submit 096

---
+++ 剑指Offer-II/剑指OfferII_096_字符串交织.go
@@ -0,0 +1,44 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/20 22:43:57
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 剑指OfferII_096_字符串交织.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	s1 := "aabcc"
+	s2 := "dbbca"
+	s3 := "aadbbcbcac"
+	fmt.Println(isInterleave(s1, s2, s3))
+}
+
+func isInterleave(s1 string, s2 string, s3 string) bool {
+	len1, len2, len3 := len(s1), len(s2), len(s3)
+	if len1+len2 != len3 {
+		return false
+	}
+	dp := make([][]bool, len1+1)
+	for i := range dp {
+		dp[i] = make([]bool, len2+1)
+	}
+	dp[0][0] = true
+	for i := 1; i <= len1; i++ {
+		dp[i][0] = dp[i-1][0] && s1[i-1] == s3[i-1]
+	}
+	for i := 1; i <= len2; i++ {
+		dp[0][i] = dp[0][i-1] && s2[i-1] == s3[i-1]
+	}
+	for i := 1; i <= len1; i++ {
+		for j := 1; j <= len2; j++ {
+			dp[i][j] = (dp[i][j-1] && s2[j-1] == s3[i+j-1]) || (dp[i-1][j] && s1[i-1] == s3[i+j-1])
+		}
+	}
+	return dp[len1][len2]
+}
