28b4433920ab85ebf3c7177ad242416b6b2c6af9
许继元 <490601115@qq.com>
2022-03-20 11:36:13 +0800

    feat: submit 021

--- hot100/021_合并两个有序链表.go
+++ hot100/021_合并两个有序链表.go
@@ -10,16 +10,14 @@
 
 package main
 
-import "fmt"
-
-type ListNode021 struct {
-	Val  int
-	Next *ListNode021
-}
+import (
+	. "LeetCodeGiyn/utils/linked-list"
+	"fmt"
+)
 
 func main() {
-	list1 := &ListNode021{1, &ListNode021{2, &ListNode021{4, nil}}}
-	list2 := &ListNode021{1, &ListNode021{3, &ListNode021{4, nil}}}
+	list1 := NewListNode([]int{1, 2, 4})
+	list2 := NewListNode([]int{1, 3, 4})
 	res := mergeTwoLists(list1, list2)
 	for res != nil {
 		fmt.Println(res.Val)
@@ -27,27 +25,7 @@ func main() {
 	}
 }
 
-func mergeTwoLists(list1 *ListNode021, list2 *ListNode021) (ans *ListNode021) {
-	// 迭代
-	//ans = &ListNode021{}
-	//cur := ans
-	//for list1 != nil && list2 != nil {
-	//	if list1.Val <= list2.Val {
-	//		cur.Next = &ListNode021{list1.Val, nil}
-	//		list1 = list1.Next
-	//	} else {
-	//		cur.Next = &ListNode021{list2.Val, nil}
-	//		list2 = list2.Next
-	//	}
-	//	cur = cur.Next
-	//}
-	//if list1 != nil {
-	//	cur.Next = list1
-	//} else {
-	//	cur.Next = list2
-	//}
-	//return ans.Next
-
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) (ans *ListNode) {
 	// 递归
 	if list1 == nil {
 		return list2
@@ -60,4 +38,24 @@ func mergeTwoLists(list1 *ListNode021, list2 *ListNode021) (ans *ListNode021) {
 		list2.Next = mergeTwoLists(list1, list2.Next)
 		return list2
 	}
+
+	// 迭代
+	//ans = &ListNode{}
+	//cur := ans
+	//for list1 != nil && list2 != nil {
+	//	if list1.Val <= list2.Val {
+	//		cur.Next = &ListNode{list1.Val, nil}
+	//		list1 = list1.Next
+	//	} else {
+	//		cur.Next = &ListNode{list2.Val, nil}
+	//		list2 = list2.Next
+	//	}
+	//	cur = cur.Next
+	//}
+	//if list1 != nil {
+	//	cur.Next = list1
+	//} else {
+	//	cur.Next = list2
+	//}
+	//return ans.Next
 }
