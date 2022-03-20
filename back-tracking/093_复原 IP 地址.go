d037f5c7161d86886a6ff542816ce271a92ea419
许继元 <490601115@qq.com>
2022-03-20 11:36:25 +0800

    feat: submit 093

--- back-tracking/093_复原 IP 地址.go
+++ back-tracking/093_复原 IP 地址.go
@@ -22,35 +22,33 @@ func main() {
 }
 
 func restoreIpAddresses(s string) (ans []string) {
+	if len(s) > 4*3 {
+		return
+	}
 	var path []string
-
-	var backtrack func(index int)
-	backtrack = func(index int) {
+	isIpAddresses := func(s string) bool {
+		if s[0] == '0' && len(s) > 1 {
+			return false
+		}
+		if tmp, _ := strconv.Atoi(s); tmp > 255 {
+			return false
+		}
+		return true
+	}
+	var dfs func(index int)
+	dfs = func(index int) {
 		if index == len(s) && len(path) == 4 {
 			ans = append(ans, strings.Join(path, "."))
-			return
 		}
 		for i := index; i < len(s); i++ {
-			if isIpAddresses(s[index : i+1]) {
-				path = append(path, s[index:i+1])
-				backtrack(i + 1)
-			} else {
+			if !isIpAddresses(s[index : i+1]) {
 				return
 			}
+			path = append(path, s[index:i+1])
+			dfs(i + 1)
 			path = path[:len(path)-1]
 		}
 	}
-	backtrack(0)
+	dfs(0)
 	return
-}
-
-func isIpAddresses(s string) bool {
-	if s[0] == '0' && len(s) > 1 {
-		return false
-	}
-	tmp, _ := strconv.Atoi(s)
-	if tmp > 255 {
-		return false
-	}
-	return true
 }
