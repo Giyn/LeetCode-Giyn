c3ef2430a1f938168a0c74c3ee150b2a78535c5b
许继元 <490601115@qq.com>
2022-03-21 00:24:09 +0800

    feat: submit 099

---
+++ 剑指Offer-II/剑指OfferII_099_最小路径之和.go
@@ -0,0 +1,42 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/21 0:01:57
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 剑指OfferII_099_最小路径之和.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	. "LeetCodeGiyn/utils/math"
+	"fmt"
+)
+
+func main() {
+	grid := [][]int{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}}
+	fmt.Println(minPathSum(grid))
+}
+
+func minPathSum(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	dp := make([][]int, m)
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+	dp[0][0] = grid[0][0]
+	for i := 1; i < m; i++ {
+		dp[i][0] = dp[i-1][0] + grid[i][0]
+	}
+	for j := 1; j < n; j++ {
+		dp[0][j] = dp[0][j-1] + grid[0][j]
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = Min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+		}
+	}
+	return dp[m-1][n-1]
+}
