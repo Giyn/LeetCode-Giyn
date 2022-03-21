c5a28cb05e7fe57fa5e89c27e4ee596915c6aff4
许继元 <490601115@qq.com>
2022-03-21 21:46:01 +0800

    feat: submit 104

---
+++ 剑指Offer-II/剑指OfferII_104_排列的数目.go
@@ -0,0 +1,32 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/21 10:25:07
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 剑指OfferII_104_排列的数目.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3}
+	target := 4
+	fmt.Println(combinationSum4(nums, target))
+}
+
+func combinationSum4(nums []int, target int) int {
+	dp := make([]int, target+1)
+	dp[0] = 1
+	for i := 0; i <= target; i++ {
+		for j := 0; j < len(nums); j++ {
+			if i >= nums[j] {
+				dp[i] += dp[i-nums[j]]
+			}
+		}
+	}
+	return dp[target]
+}
