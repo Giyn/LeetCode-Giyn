dfecf5ad2db9772e969a8f3668f8045b051d0420
许继元 <490601115@qq.com>
2022-03-24 10:53:12 +0800

    feat: submit 725

--- linked-list/725_分隔链表.go
+++ linked-list/725_分隔链表.go
@@ -10,27 +10,13 @@
 
 package main
 
-import "fmt"
-
-type ListNode725 struct {
-	Val  int
-	Next *ListNode725
-}
+import (
+	. "LeetCodeGiyn/utils/linked-list"
+	"fmt"
+)
 
 func main() {
-	listNode10 := ListNode725{10, nil}
-	listNode9 := ListNode725{9, &listNode10}
-	listNode8 := ListNode725{8, &listNode9}
-	listNode7 := ListNode725{7, &listNode8}
-	listNode6 := ListNode725{6, &listNode7}
-	listNode5 := ListNode725{5, &listNode6}
-	listNode4 := ListNode725{4, &listNode5}
-	listNode3 := ListNode725{3, &listNode4}
-	listNode2 := ListNode725{2, &listNode3}
-	listNode1 := ListNode725{1, &listNode2}
-
-	head := &listNode1
-
+	head := NewListNode([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
 	res := splitListToParts(head, 3)
 	for _, i := range res {
 		for i != nil {
@@ -41,13 +27,13 @@ func main() {
 	}
 }
 
-func splitListToParts(head *ListNode725, k int) []*ListNode725 {
+func splitListToParts(head *ListNode, k int) []*ListNode {
 	n := 0
 	for node := head; node != nil; node = node.Next {
 		n++
 	}
 	base, rest := n/k, n%k
-	parts := make([]*ListNode725, k)
+	parts := make([]*ListNode, k)
 	for i, curr := 0, head; i < k && curr != nil; i++ {
 		parts[i] = curr
 		partSize := base
