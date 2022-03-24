0640d3983dfd7e4ad33faee8411c7a79a1476701
许继元 <490601115@qq.com>
2022-03-24 10:52:56 +0800

    feat: submit 203

--- linked-list/203_移除链表元素.go
+++ linked-list/203_移除链表元素.go
@@ -10,18 +10,16 @@
 
 package main
 
-import "fmt"
-
-type ListNode203 struct {
-	Val  int
-	Next *ListNode203
-}
+import (
+	. "LeetCodeGiyn/utils/linked-list"
+	"fmt"
+)
 
 func main() {
-	listNode4 := ListNode203{4, nil}
-	listNode3 := ListNode203{3, &listNode4}
-	listNode2 := ListNode203{2, &listNode3}
-	listNode1 := ListNode203{1, &listNode2}
+	listNode4 := ListNode{Val: 4}
+	listNode3 := ListNode{Val: 3, Next: &listNode4}
+	listNode2 := ListNode{Val: 2, Next: &listNode3}
+	listNode1 := ListNode{Val: 1, Next: &listNode2}
 
 	head := &listNode1
 
@@ -32,8 +30,8 @@ func main() {
 	}
 }
 
-func removeElements(head *ListNode203, val int) *ListNode203 {
-	fake := new(ListNode203)
+func removeElements(head *ListNode, val int) *ListNode {
+	fake := new(ListNode)
 	fake.Next = head
 	cur := fake
 	for cur != nil && cur.Next != nil {
