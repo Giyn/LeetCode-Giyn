26eca16da3e456aa7b3a2c61b859fa1e124a80a9
许继元 <490601115@qq.com>
2022-03-24 10:43:58 +0800

    feat: submit 440

---
+++ per-day-one-question/440_字典序的第K小数字.go
@@ -0,0 +1,47 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/23 0:03:24
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 440_字典序的第K小数字.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	. "LeetCodeGiyn/utils/math"
+	"fmt"
+)
+
+func main() {
+	n := 13
+	k := 2
+	fmt.Println(findKthNumber(n, k))
+}
+
+func findKthNumber(n int, k int) int {
+	getSteps := func(cur, n int) (steps int) {
+		first, last := cur, cur
+		for first <= n {
+			steps += Min(last, n) - first + 1
+			first *= 10
+			last = last*10 + 9
+		}
+		return
+	}
+	cur := 1
+	k--
+	for k > 0 {
+		steps := getSteps(cur, n)
+		if steps <= k {
+			k -= steps
+			cur++
+		} else {
+			cur *= 10
+			k--
+		}
+	}
+	return cur
+}
