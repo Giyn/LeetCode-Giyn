436f39d88fadfef45c2d434daf7861fe8cc41246
许继元 <490601115@qq.com>
2022-03-26 11:14:56 +0800

    feat: submit 018

---
+++ 剑指Offer/剑指Offer_018_删除链表的节点.go
@@ -0,0 +1,42 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/25 19:59:57
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 剑指Offer_018_删除链表的节点.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	. "LeetCodeGiyn/utils/linked-list"
+	"fmt"
+)
+
+func main() {
+	head := NewListNode([]int{4, 5, 1, 9})
+	val := 5
+	ans := deleteNode(head, val)
+	for ans != nil {
+		fmt.Println(ans.Val)
+		ans = ans.Next
+	}
+}
+
+func deleteNode(head *ListNode, val int) *ListNode {
+	dummy := &ListNode{Next: head}
+	cur := dummy
+	for cur != nil && cur.Next != nil {
+		if cur.Next.Val == val {
+			if cur.Next.Next != nil {
+				cur.Next = cur.Next.Next
+			} else {
+				cur.Next = nil
+			}
+		}
+		cur = cur.Next
+	}
+	return dummy.Next
+}
