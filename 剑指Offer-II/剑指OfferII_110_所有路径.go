894c96d6c6d4bdae03e2a7a3da65bd3a6f19ec2f
许继元 <490601115@qq.com>
2022-03-28 22:32:58 +0800

    feat: submit 110

---
+++ 剑指Offer-II/剑指OfferII_110_所有路径.go
@@ -0,0 +1,36 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/28 15:43:24
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 剑指OfferII_110_所有路径.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	graph := [][]int{{4, 3, 1}, {3, 2, 4}, {3}, {4}, {}}
+	fmt.Println(allPathsSourceTarget(graph))
+}
+
+func allPathsSourceTarget(graph [][]int) (ans [][]int) {
+	path := []int{0}
+	var dfs func(x int)
+	dfs = func(x int) {
+		if x == len(graph)-1 {
+			ans = append(ans, append([]int{}, path...))
+			return
+		}
+		for _, y := range graph[x] {
+			path = append(path, y)
+			dfs(y)
+			path = path[:len(path)-1]
+		}
+	}
+	dfs(0)
+	return
+}
