6b61aa7e2f8207f6f0418b798d1e5c640c489b98
许继元 <490601115@qq.com>
2022-03-28 22:32:51 +0800

    feat: submit 107

---
+++ 剑指Offer-II/剑指OfferII_107_矩阵中的距离.go
@@ -0,0 +1,45 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/28 21:08:20
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 剑指OfferII_107_矩阵中的距离.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	mat := [][]int{{0, 0, 0}, {0, 1, 0}, {1, 1, 1}}
+	fmt.Println(updateMatrix(mat))
+}
+
+func updateMatrix(mat [][]int) [][]int {
+	m, n := len(mat), len(mat[0])
+	var queue [][]int
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if mat[i][j] == 0 {
+				queue = append(queue, []int{i, j})
+			} else {
+				mat[i][j] = -1
+			}
+		}
+	}
+	dirs := [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+		for _, dir := range dirs {
+			x, y := node[0]+dir[0], node[1]+dir[1]
+			if x >= 0 && x < m && y >= 0 && y < n && mat[x][y] == -1 {
+				mat[x][y] = mat[node[0]][node[1]] + 1
+				queue = append(queue, []int{x, y})
+			}
+		}
+	}
+	return mat
+}
