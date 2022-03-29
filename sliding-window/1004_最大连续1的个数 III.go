8a93f5e18e1ca759712b2eb036f5da461b9661d1
许继元 <490601115@qq.com>
2022-03-29 11:17:25 +0800

    feat: submit 1004

---
+++ sliding-window/1004_最大连续1的个数 III.go
@@ -0,0 +1,39 @@
+/*
+-------------------------------------
+# @Time    : 2022/3/29 9:59:02
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 1004_最大连续1的个数 III.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	. "LeetCodeGiyn/utils/math"
+	"fmt"
+)
+
+func main() {
+	nums := []int{0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1}
+	k := 3
+	fmt.Println(longestOnes(nums, k))
+}
+
+func longestOnes(nums []int, k int) (ans int) {
+	left, cnt := 0, 0
+	for right := range nums {
+		if nums[right] != 1 {
+			cnt++
+		}
+		for cnt > k {
+			if nums[left] != 1 {
+				cnt--
+			}
+			left++
+		}
+		ans = Max(ans, right-left+1)
+	}
+	return
+}
