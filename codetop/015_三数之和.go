2a145e1cc99552526aa05c51bf6a041f1a57e24b
许继元 <490601115@qq.com>
2022-04-04 01:17:47 +0800

    feat: submit 015

---
+++ codetop/015_三数之和.go
@@ -0,0 +1,53 @@
+/*
+-------------------------------------
+# @Time    : 2021/11/2 16:16:14
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 015_三数之和.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	nums := []int{-1, 0, 1, 2, -1, -4}
+	//nums := []int{0, 0, 0, 0, 0, 0}
+	fmt.Println(threeSum(nums))
+}
+
+func threeSum(nums []int) (ans [][]int) {
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-2; i++ {
+		n1 := nums[i]
+		if n1 > 0 {
+			break
+		}
+		if i > 0 && n1 == nums[i-1] {
+			continue
+		}
+		left, right := i+1, len(nums)-1
+		for left < right {
+			n2, n3 := nums[left], nums[right]
+			if n1+n2+n3 == 0 {
+				ans = append(ans, []int{n1, n2, n3})
+				for left < right && nums[left] == n2 {
+					left++
+				}
+				for left < right && nums[right] == n3 {
+					right--
+				}
+			} else if nums[i]+nums[left]+nums[right] < 0 {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+	return
+}
