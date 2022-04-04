0482435fa550f72f5fb69721eb0fb77ab8b78276
许继元 <490601115@qq.com>
2022-04-05 00:59:15 +0800

    feat: submit 459

--- string/459_重复的子字符串.go
+++ string/459_重复的子字符串.go
@@ -19,35 +19,29 @@ func main() {
 	fmt.Println(repeatedSubstringPattern(s))
 }
 
-func getNext459(next []int, s string) {
-	j := 0
-	next[0] = j
-	for i := 1; i < len(s); i++ {
-		for j > 0 && s[i] != s[j] {
-			j = next[j-1]
-		}
-		if s[i] == s[j] {
-			j++
-		}
-		next[i] = j
-	}
-}
-
 func repeatedSubstringPattern(s string) bool {
+	getNext := func(next []int, s string) {
+		j := 0
+		next[0] = j
+		for i := 1; i < len(s); i++ {
+			for j > 0 && s[i] != s[j] {
+				j = next[j-1]
+			}
+			if s[i] == s[j] {
+				j++
+			}
+			next[i] = j
+		}
+	}
 	n := len(s)
 	if n == 0 {
 		return false
 	}
 	next := make([]int, n)
-	getNext459(next, s)
+	getNext(next, s)
 	// 数组长度减去最长相同前后缀的长度相当于是第一个周期的长度，也就是一个周期的长度，如果这个周期可以被整除，就说明整个数组就是这个周期的循环。
 	if next[n-1] != 0 && n%(n-next[n-1]) == 0 {
 		return true
 	}
 	return false
 }
-
-//func repeatedSubstringPattern(s string) bool {
-//	ss := s + s
-//	return strings.Contains(ss[1:len(ss)-1], s)
-//}
