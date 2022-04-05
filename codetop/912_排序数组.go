6caaf69af097ff907392d21ec0378426b94456db
许继元 <490601115@qq.com>
2022-04-05 19:25:47 +0800

    feat: submit 912

--- codetop/912_排序数组.go
+++ codetop/912_排序数组.go
@@ -18,30 +18,56 @@ func main() {
 }
 
 func sortArray(nums []int) []int {
-	var quickSort func(s []int, left, right int)
-	quickSort = func(s []int, left, right int) {
-		i, j := left, right
-		pivot := s[(left+right)/2]
-		for i <= j {
-			for s[i] < pivot {
-				i++
-			}
-			for s[j] > pivot {
-				j--
-			}
-			if i <= j {
-				s[i], s[j] = s[j], s[i]
-				i++
-				j--
+	//var quickSort func(s []int, left, right int)
+	//quickSort = func(s []int, left, right int) {
+	//	i, j := left, right
+	//	pivot := s[(left+right)/2]
+	//	for i <= j {
+	//		for s[i] < pivot {
+	//			i++
+	//		}
+	//		for s[j] > pivot {
+	//			j--
+	//		}
+	//		if i <= j {
+	//			s[i], s[j] = s[j], s[i]
+	//			i++
+	//			j--
+	//		}
+	//	}
+	//	if j > left {
+	//		quickSort(s, left, j)
+	//	}
+	//	if i < right {
+	//		quickSort(s, i, right)
+	//	}
+	//}
+	var mergeSort func(left, right []int) (ans []int)
+	mergeSort = func(left, right []int) (ans []int) {
+		for len(left) > 0 && len(right) > 0 {
+			if left[0] <= right[0] {
+				ans = append(ans, left[0])
+				left = left[1:]
+			} else {
+				ans = append(ans, right[0])
+				right = right[1:]
 			}
 		}
-		if j > left {
-			quickSort(s, left, j)
+		for len(left) > 0 {
+			ans = append(ans, left[0])
+			left = left[1:]
 		}
-		if i < right {
-			quickSort(s, i, right)
+		for len(right) > 0 {
+			ans = append(ans, right[0])
+			right = right[1:]
 		}
+		return
 	}
-	quickSort(nums, 0, len(nums)-1)
-	return nums
+	//quickSort(nums, 0, len(nums)-1)
+	if len(nums) < 2 {
+		return nums
+	}
+	mid := len(nums) / 2
+	//return nums
+	return mergeSort(sortArray(nums[:mid]), sortArray(nums[mid:]))
 }
