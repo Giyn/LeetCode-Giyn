568944b6fa37888970fb6ff9687be6e05c7e7d20
许继元 <490601115@qq.com>
2022-04-06 01:19:05 +0800

    feat: submit 072

---
+++ codetop/072_编辑距离.go
@@ -0,0 +1,43 @@
+/*
+-------------------------------------
+# @Time    : 2022/2/24 10:18:31
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 072_编辑距离.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	. "LeetCodeGiyn/utils/math"
+	"fmt"
+)
+
+func main() {
+	word1 := "horse"
+	word2 := "ros"
+	fmt.Println(editDistance(word1, word2))
+}
+
+func editDistance(word1 string, word2 string) int {
+	dp := make([][]int, len(word1)+1)
+	for i := range dp {
+		dp[i] = make([]int, len(word2)+1)
+		dp[i][0] = i
+	}
+	for j := range dp[0] {
+		dp[0][j] = j
+	}
+	for i := 1; i <= len(word1); i++ {
+		for j := 1; j <= len(word2); j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = Min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1 // 删除(包括添加)或者替换
+			}
+		}
+	}
+	return dp[len(word1)][len(word2)]
+}
