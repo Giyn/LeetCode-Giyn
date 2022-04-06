80462d5db0bfda4eb1c015b7091beddd68fc8ad3
许继元 <490601115@qq.com>
2022-04-06 20:29:12 +0800

    feat: submit 153

---
+++ codetop/153_寻找旋转排序数组中的最小值.go
@@ -0,0 +1,32 @@
+/*
+-------------------------------------
+# @Time    : 2022/4/6 17:04:26
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 153_寻找旋转排序数组中的最小值.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{3, 4, 5, 1, 2}
+	fmt.Println(findMin(nums))
+}
+
+func findMin(nums []int) int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := left + (right-left)>>1
+		// 选择无序的部分
+		if nums[mid] < nums[right] {
+			right = mid // 若mid-1，则丢失了最小值
+		} else {
+			left = mid + 1
+		}
+	}
+	return nums[left]
+}
