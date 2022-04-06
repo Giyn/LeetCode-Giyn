828f415073583669b5bb84c2f8b41e8ba0063dba
许继元 <490601115@qq.com>
2022-04-07 01:39:40 +0800

    feat: submit 009

---
+++ codetop/009_回文数.go
@@ -0,0 +1,32 @@
+/*
+-------------------------------------
+# @Time    : 2022/4/6 20:31:50
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 009_回文数.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 1234321
+	fmt.Println(isPalindrome(x))
+}
+
+func isPalindrome(x int) bool {
+	// 没有前导0
+	if (x < 0) || (x != 0 && x%10 == 0) {
+		return false
+	}
+	// 反转右半部分
+	reversed := 0
+	for x > reversed {
+		reversed = 10*reversed + x%10
+		x /= 10
+	}
+	return x == reversed || x == reversed/10
+}
