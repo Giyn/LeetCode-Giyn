05644522126f72c0bf22a1986553bba9f1a5fa19
许继元 <490601115@qq.com>
2022-04-09 16:12:09 +0800

    feat: submit 058

---
+++ codetop/剑指Offer_058_I_翻转单词顺序.go
@@ -0,0 +1,41 @@
+/*
+-------------------------------------
+# @Time    : 2022/4/8 23:03:36
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 剑指Offer_058_I_翻转单词顺序.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	s := "  hello world!  "
+	fmt.Println(reverseWordsJZ(s))
+}
+
+func reverseWordsJZ(s string) string {
+	var ans []string
+	n := len(s)
+	for left, right := n-1, n-1; left >= 0 && right >= 0; {
+		for right >= 0 && s[right] == ' ' {
+			right--
+		}
+		if right == -1 {
+			break
+		}
+		left = right
+		for left >= 0 && s[left] != ' ' {
+			left--
+		}
+		ans = append(ans, s[left+1:right+1])
+		right = left
+	}
+	return strings.Join(ans, " ")
+}
