4e694bb1c5c8f23f6019e1e7870d346c1511ee12
许继元 <490601115@qq.com>
2022-04-09 16:11:56 +0800

    feat: submit 406

---
+++ codetop/406_根据身高重建队列.go
@@ -0,0 +1,41 @@
+/*
+-------------------------------------
+# @Time    : 2021/12/19 10:16:14
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 406_根据身高重建队列.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	people := [][]int{{7, 0}, {7, 1}, {6, 1}, {5, 0}, {5, 2}, {4, 4}}
+	fmt.Println(reconstructQueue(people))
+}
+
+func reconstructQueue(people [][]int) (ans [][]int) {
+	sort.Slice(people, func(i, j int) bool {
+		if people[i][0] == people[j][0] {
+			return people[i][1] < people[j][1]
+		}
+		return people[i][0] > people[j][0]
+	})
+	// 按序号插入
+	// 如: [[7 0] [7 1] [6 1] [5 0] [5 2] [4 4]]
+	// 第一步: [[7 0] [7 1] [6 1] [5 0] [5 2] [4 4]]
+	// 第二步: [[7 0] [7 1] [6 1] [5 0] [5 2] [4 4]]
+	// 第三步: [[7 0] [6 1] [7 1] [5 0] [5 2] [4 4]]
+	for _, info := range people {
+		ans = append(ans, info)
+		copy(ans[info[1]+1:], ans[info[1]:])
+		ans[info[1]] = info
+	}
+	return
+}
