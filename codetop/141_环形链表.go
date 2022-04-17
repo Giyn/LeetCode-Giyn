f34616c8408d806dc7ed160d9d05d14bb182cddf
许继元 <490601115@qq.com>
2022-04-17 15:05:07 +0800

    feat: submit 141

---
+++ codetop/141_环形链表.go
@@ -0,0 +1,34 @@
+/*
+-------------------------------------
+# @Time    : 2022/4/17 14:59:42
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 141_环形链表.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	. "LeetCodeGiyn/utils/linked-list"
+	"fmt"
+)
+
+func main() {
+	head := NewListNode([]int{3, 2, 0})
+	head.Next.Next.Next = &ListNode{Val: -4, Next: head.Next}
+	fmt.Println(hasCycle(head))
+}
+
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
+	for slow != nil && fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
