b81fa8ad6e41bb2fb50ab3c4e82b29d59e93c5a7
许继元 <490601115@qq.com>
2022-04-17 22:54:05 +0800

    feat: submit 110

--- binary-tree/110_平衡二叉树.go
+++ binary-tree/110_平衡二叉树.go
@@ -17,7 +17,7 @@ import (
 )
 
 func main() {
-	root := &TreeNode{3, &TreeNode{9, nil, nil}, &TreeNode{20, &TreeNode{15, nil, nil}, &TreeNode{7, nil, nil}}}
+	root := &TreeNode{Val: 3, Left: &TreeNode{Val: 9}, Right: &TreeNode{Val: 20, Left: &TreeNode{Val: 15}, Right: &TreeNode{Val: 7}}}
 	fmt.Println(isBalanced(root))
 }
 
