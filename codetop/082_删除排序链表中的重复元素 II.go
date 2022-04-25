adf8f941a33a39fe17deb8582d2537b04e4503fb
许继元 <490601115@qq.com>
2022-04-25 20:05:14 +0800

    feat: submit 082

---
+++ codetop/082_删除排序链表中的重复元素 II.go
@@ -0,0 +1,44 @@
+/*
+-------------------------------------
+# @Time    : 2022/4/25 17:22:56
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 082_删除排序链表中的重复元素 II.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	. "LeetCodeGiyn/utils/linked-list"
+	"fmt"
+)
+
+func main() {
+	head := NewListNode([]int{1, 2, 2, 3, 3, 4, 5})
+	ans := deleteDuplicates(head)
+	for ans != nil {
+		fmt.Println(ans.Val)
+		ans = ans.Next
+	}
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	dummy := &ListNode{Next: head}
+	cur := dummy
+	for cur.Next != nil && cur.Next.Next != nil {
+		if cur.Next.Val == cur.Next.Next.Val {
+			x := cur.Next.Val
+			for cur.Next != nil && cur.Next.Val == x {
+				cur.Next = cur.Next.Next
+			}
+		} else {
+			cur = cur.Next
+		}
+	}
+	return dummy.Next
+}
