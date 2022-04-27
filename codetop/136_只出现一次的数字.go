3da4abbda498020d4f34cccc1f84b2d6df629852
许继元 <490601115@qq.com>
2022-04-27 18:02:35 +0800

    feat: submit 136

---
+++ codetop/136_只出现一次的数字.go
@@ -0,0 +1,26 @@
+/*
+-------------------------------------
+# @Time    : 2022/4/26 18:30:06
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 136_只出现一次的数字.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{2, 2, 1}
+	fmt.Println(singleNumber(nums))
+}
+
+func singleNumber(nums []int) int {
+	ans := nums[0]
+	for _, num := range nums[1:] {
+		ans ^= num
+	}
+	return ans
+}
