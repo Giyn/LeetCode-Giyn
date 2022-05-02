55249c3da486b34cc20912847cb8bf78b29c09e2
许继元 <490601115@qq.com>
2022-05-03 01:44:19 +0800

    feat: submit 301

--- per-day-one-question/301_删除无效的括号.go
+++ per-day-one-question/301_删除无效的括号.go
@@ -19,6 +19,44 @@ func main() {
 }
 
 func removeInvalidParentheses(str string) (ans []string) {
+	isValid := func(str string) bool {
+		cnt := 0
+		for _, ch := range str {
+			if ch == '(' {
+				cnt++
+			} else if ch == ')' {
+				cnt--
+				if cnt < 0 {
+					return false
+				}
+			}
+		}
+		return cnt == 0
+	}
+	var dfs func(str string, start, l, r int, ans *[]string)
+	dfs = func(str string, start, l, r int, ans *[]string) {
+		if l == 0 && r == 0 {
+			if isValid(str) {
+				*ans = append(*ans, str)
+			}
+			return
+		}
+		for i := start; i < len(str); i++ {
+			// 多种相同情况只需去除一次
+			if i != start && str[i] == str[i-1] {
+				continue
+			}
+			if str[i] == ')' && r > 0 {
+				cur := str
+				cur = cur[:i] + cur[i+1:]
+				dfs(cur, i, l, r-1, ans)
+			} else if str[i] == '(' && l > 0 {
+				cur := str
+				cur = cur[:i] + cur[i+1:]
+				dfs(cur, i, l-1, r, ans)
+			}
+		}
+	}
 	l, r := 0, 0
 	for _, v := range str {
 		if v == '(' {
@@ -31,47 +69,6 @@ func removeInvalidParentheses(str string) (ans []string) {
 			}
 		}
 	}
-	dfs301(str, 0, l, r, &ans)
+	dfs(str, 0, l, r, &ans)
 	return
-}
-
-func isValid(str string) bool {
-	cnt := 0
-	for _, ch := range str {
-		if ch == '(' {
-			cnt++
-		} else if ch == ')' {
-			cnt--
-			if cnt < 0 {
-				return false
-			}
-		}
-	}
-	return cnt == 0
-}
-
-func dfs301(str string, start, l, r int, ans *[]string) {
-	if l == 0 && r == 0 {
-		if isValid(str) {
-			*ans = append(*ans, str)
-		}
-		return
-	}
-
-	for i := start; i < len(str); i++ {
-		// 多种相同情况只需去除一次
-		if i != start && str[i] == str[i-1] {
-			continue
-		}
-
-		if str[i] == ')' && r > 0 {
-			cur := str
-			cur = cur[:i] + cur[i+1:]
-			dfs301(cur, i, l, r-1, ans)
-		} else if str[i] == '(' && l > 0 {
-			cur := str
-			cur = cur[:i] + cur[i+1:]
-			dfs301(cur, i, l-1, r, ans)
-		}
-	}
 }
