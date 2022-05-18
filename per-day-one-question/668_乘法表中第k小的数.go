0b99628ed45d671348b048af423df9b31912cd99
许继元 <490601115@qq.com>
2022-05-18 16:12:14 +0800

    feat: submit 668

---
+++ per-day-one-question/668_乘法表中第k小的数.go
@@ -0,0 +1,33 @@
+/*
+-------------------------------------
+# @Time    : 2022/5/18 0:58:37
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 668_乘法表中第k小的数.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	m := 3
+	n := 3
+	k := 5
+	fmt.Println(findKthNumber668(m, n, k))
+}
+
+func findKthNumber668(m, n, k int) int {
+	return sort.Search(m*n, func(x int) bool {
+		count := x / n * n
+		for i := x/n + 1; i <= m; i++ {
+			count += x / i
+		}
+		return count >= k
+	})
+}
