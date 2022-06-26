b13ddaf3794fb91f3500ac0d7f3a0977794f89ba
许继元 <490601115@qq.com>
2022-06-27 03:45:40 +0800

    feat: submit 112

---
+++ codetop/112_路径总和/112_路径总和.go
@@ -0,0 +1,32 @@
+/*
+-------------------------------------
+# @Time    : 2022/6/25 21:19:02
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 112_路径总和.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import (
+	. "LeetCodeGiyn/utils"
+	"fmt"
+)
+
+func main() {
+	root := &TreeNode{Val: 5, Left: &TreeNode{Val: 4, Left: &TreeNode{Val: 11, Left: &TreeNode{Val: 7}, Right: &TreeNode{Val: 2}}}, Right: &TreeNode{Val: 8, Left: &TreeNode{Val: 13}, Right: &TreeNode{Val: 4, Right: &TreeNode{Val: 1}}}}
+	targetSum := 22
+	fmt.Println(hasPathSum(root, targetSum))
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil {
+		return targetSum == root.Val
+	}
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+}
