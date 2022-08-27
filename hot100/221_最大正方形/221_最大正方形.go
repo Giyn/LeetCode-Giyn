3293986572d7a5773bd044d3ca0e10ad8cd47696
许继元 <490601115@qq.com>
2022-08-27 23:36:53 +0800

    feat: submit 221

---
+++ hot100/221_最大正方形/221_最大正方形.go
@@ -0,0 +1,51 @@
+/*
+-------------------------------------
+# @Time    : 2022/8/27 22:01:42
+# @Author  : Giyn
+# @Email   : giyn.jy@gmail.com
+# @File    : 221_最大正方形.go
+# @Software: GoLand
+-------------------------------------
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	matrix := [][]byte{{'1', '0', '1', '0', '0'}, {'1', '0', '1', '1', '1'}, {'1', '1', '1', '1', '1'}, {'1', '0', '0', '1', '0'}}
+	fmt.Println(maximalSquare(matrix))
+}
+
+func maximalSquare(matrix [][]byte) int {
+	dp := make([][]int, len(matrix))
+	maxSide := 0
+	for i := 0; i < len(matrix); i++ {
+		dp[i] = make([]int, len(matrix[i]))
+		for j := 0; j < len(matrix[i]); j++ {
+			dp[i][j] = int(matrix[i][j] - '0')
+			if dp[i][j] == 1 {
+				maxSide = 1
+			}
+		}
+	}
+
+	for i := 1; i < len(matrix); i++ {
+		for j := 1; j < len(matrix[i]); j++ {
+			if dp[i][j] == 1 {
+				dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1
+				if dp[i][j] > maxSide {
+					maxSide = dp[i][j]
+				}
+			}
+		}
+	}
+	return maxSide * maxSide
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
